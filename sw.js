1eb37bdb7fceef8ba7bcf33182802617dc6dbb3a
ologoligature <victimazza@gmail.com>
2026-05-14 19:40:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-// Service Worker Minimalis biar syarat PWA terpenuhi
-self.addEventListener('install', (event) => {
-  console.log('SW: Install Event');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('SW: Activate Event');
-});
-
-// Wajib ada fetch event biar Chrome nganggep ini PWA valid
-self.addEventListener('fetch', (event) => {
-  // Biarkan request lewat secara normal
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
